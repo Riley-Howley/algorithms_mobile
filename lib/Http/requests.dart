6cc72ddccca49ff75ec1474c1a70b2e651bcdba6
Riley Howley <71965268+Riley-Howley@users.noreply.github.com>
2022-07-03 02:07:52 +1200

    Added connectivity Socket and Post Request

--- lib/Http/requests.dart
+++ lib/Http/requests.dart
@@ -9,6 +9,8 @@ HttpClient client = new HttpClient();
 //String local = "https://10.0.2.2:7234/api";
 var ip = "http://developer.kensnz.com/getlocdata";
 
+String code = "";
+
 List<CoordModel> list = [];
 
 Future getAllData(String id) async {
@@ -45,13 +47,22 @@ Future getAllData(String id) async {
   print(list);
 }
 
-Future postAllData(String id, String descriotion) async {
+Future postAllData(String userid, String description) async {
+  Map map = {
+    "userid": "$userid",
+    "latitude": "$latitude",
+    "longitude": "$log",
+    "description": "$description",
+  };
   client.badCertificateCallback =
       ((X509Certificate cert, String host, int port) => true);
-  HttpClientRequest request = await client.postUrl(Uri.parse(
-      "http://developer.kensnz.com/api/addlocdata?userid=$id&latitude=$latitude&longitide=$log&description=$descriotion"));
+  HttpClientRequest request = await client
+      .postUrl(Uri.parse("http://developer.kensnz.com/api/addlocdata"));
   request.headers.add("Content-Type", "application/json");
   request.headers.add("Accept", "*/*");
+  request.add(utf8.encode(jsonEncode(map)));
   HttpClientResponse result = await request.close();
   print(result.statusCode);
+  code = result.statusCode.toString();
+  print(code);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,162 @@
-import 'package:algorithm_mobile/dashboard_screen.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'dashboard_screen.dart';
+
+void main() => runApp(MaterialApp(home: HomePage()));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _HomePageState extends State<HomePage> {
+  Map _source = {ConnectivityResult.none: false};
+  final MyConnectivity _connectivity = MyConnectivity.instance;
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      home: const MyHomePage(),
-    );
+  void initState() {
+    super.initState();
+    _connectivity.initialise();
+    _connectivity.myStream.listen((source) {
+      setState(() => _source = source);
+    });
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   var nameController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
+    String string;
+    switch (_source.keys.toList()[0]) {
+      case ConnectivityResult.mobile:
+        string = 'Mobile: Online';
+        break;
+      case ConnectivityResult.wifi:
+        string = 'WiFi: Online';
+        break;
+      case ConnectivityResult.none:
+      default:
+        string = 'Offline';
+    }
+
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/home.jpeg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(28.0),
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Container(
-                    margin: EdgeInsets.only(top: 20),
-                    child: Text(
-                      "Location\nBased...",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold),
-                    ),
+      backgroundColor: string == "Offline" ? Colors.orange : Colors.transparent,
+      body: string == "Offline"
+          ? Center(
+              child: Text(
+                "Error! Application needs a valid network connection",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            )
+          : Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/home.jpeg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(28.0),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          margin: EdgeInsets.only(top: 20),
+                          child: Text(
+                            "Location\nBased...",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 40),
+                        child: TextField(
+                          controller: nameController,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            hintText: "Enter your name",
+                            hintStyle: TextStyle(color: Colors.white),
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          "Explore Now",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: FloatingActionButton(
+                          backgroundColor: Colors.white,
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) =>
+                                  DashboardScreen(nameController.text),
+                            ));
+                          },
+                          child: Icon(Icons.explore),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Container(
-                  margin: EdgeInsets.only(top: 40),
-                  child: TextField(
-                    controller: nameController,
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      hintText: "Enter your name",
-                      hintStyle: TextStyle(color: Colors.white),
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                Container(
-                  child: Text(
-                    "Explore Now",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Container(
-                  child: FloatingActionButton(
-                    backgroundColor: Colors.white,
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) =>
-                            DashboardScreen(nameController.text),
-                      ));
-                    },
-                    child: Icon(Icons.explore),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
+
+  @override
+  void dispose() {
+    _connectivity.disposeStream();
+    super.dispose();
+  }
+}
+
+class MyConnectivity {
+  MyConnectivity._();
+
+  static final _instance = MyConnectivity._();
+  static MyConnectivity get instance => _instance;
+  final _connectivity = Connectivity();
+  final _controller = StreamController.broadcast();
+  Stream get myStream => _controller.stream;
+
+  void initialise() async {
+    ConnectivityResult result = await _connectivity.checkConnectivity();
+    _checkStatus(result);
+    _connectivity.onConnectivityChanged.listen((result) {
+      _checkStatus(result);
+    });
+  }
+
+  void _checkStatus(ConnectivityResult result) async {
+    bool isOnline = false;
+    try {
+      final result = await InternetAddress.lookup('example.com');
+      isOnline = result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      isOnline = false;
+    }
+    _controller.sink.add({result: isOnline});
+  }
+
+  void disposeStream() => _controller.close();
 }
